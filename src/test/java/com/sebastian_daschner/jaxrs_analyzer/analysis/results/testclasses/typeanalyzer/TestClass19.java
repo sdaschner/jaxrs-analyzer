0f2e79f1adef17d9e8ce83a06195e716a66c4b11
Daryl Teo <dteo@darylteo.local>
2016-05-17 12:08:13 +1000

    Add a super super class test

---
+++ src/test/java/com/sebastian_daschner/jaxrs_analyzer/analysis/results/testclasses/typeanalyzer/TestClass19.java
@@ -0,0 +1,67 @@
+package com.sebastian_daschner.jaxrs_analyzer.analysis.results.testclasses.typeanalyzer;
+
+import com.sebastian_daschner.jaxrs_analyzer.analysis.results.TypeUtils;
+import com.sebastian_daschner.jaxrs_analyzer.model.rest.TypeIdentifier;
+import com.sebastian_daschner.jaxrs_analyzer.model.rest.TypeRepresentation;
+import com.sebastian_daschner.jaxrs_analyzer.model.types.Type;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TestClass19 extends SuperTestClass5 {
+
+    private String foobar;
+
+    public String getFoobar() {
+        return foobar;
+    }
+
+    public void setFoobar(String foobar) {
+        this.foobar = foobar;
+    }
+
+    public static Set<TypeRepresentation> expectedTypeRepresentations() {
+        final Map<String, TypeIdentifier> properties = new HashMap<>();
+
+        properties.put("foobar", TypeUtils.STRING_IDENTIFIER);
+        properties.put("test", TypeUtils.STRING_IDENTIFIER);
+        properties.put("hello", TypeUtils.STRING_IDENTIFIER);
+
+        return Collections.singleton(TypeRepresentation.ofConcrete(expectedIdentifier(), properties));
+    }
+
+    public static TypeIdentifier expectedIdentifier() {
+        return TypeIdentifier.ofType(new Type(TestClass19.class.getName()));
+    }
+
+}
+
+class SuperTestClass5 extends SuperTestClass6 {
+
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+}
+
+class SuperTestClass6 {
+
+    private String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+}
